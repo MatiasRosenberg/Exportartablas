7d8fb3bdb4cfbed89b1c9461af75383d5171785d
DELL <DELL@DESKTOP-MC6UN05>
2021-05-11 16:03:05 -0300

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Data.SqlClient;
+using System.Configuration;
+using static Conexion.Conexionbd;
+using static Conexion.Excel;
+
+
+
+namespace Conexion
+{
+    public partial class Exportar : Form
+    {
+        
+
+        public Exportar()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmb.Items.Add("Seleccione opcion");
+            cmb.Items.Add("Pedidos de clientes");
+
+            cmb.SelectedItem = cmb.Items[0];
+        }
+       
+
+
+        private void Btnexportar_Click(object sender, EventArgs e)
+        {
+            Excel ex = new Excel();
+
+            ex.ExportarDataGridViewExcel(grilla);
+            
+        }
+
+        private void Btnsalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnprocesar_Click(object sender, EventArgs e)
+        {
+            Funciones F = new Funciones();
+
+            if(cmb.SelectedIndex == 0)
+            {
+                MessageBox.Show("Seleccione una opcion");
+            }
+            else if (cmb.SelectedIndex == 1)
+            {
+                F.Pedidosdeclientes(mtxtdesde, mtxthasta, grilla);
+            }
+            
+        }
+
+        private void grilla_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            using (SolidBrush b = new SolidBrush(grilla.RowHeadersDefaultCellStyle.ForeColor))
+            {
+                e.Graphics.DrawString((e.RowIndex + 1).ToString(), e.InheritedRowStyle.Font, b, e.RowBounds.Location.X + 10, e.RowBounds.Location.Y + 4);
+            }
+        }
+
+        private void txtdesde_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+
+                e.Handled = true;
+
+                return;
+            }
+        }
+
+        private void txthasta_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+
+                e.Handled = true;
+
+                return;
+            }
+        }
+
+        private void cmb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmb.SelectedIndex == 1)
+            {
+                mtxtdesde.Visible = true;
+                lbldesde.Visible = true;
+                mtxthasta.Visible = true;
+                lblhasta.Visible = true;
+                mtxtdesde.Focus();
+            }
+            else
+            {
+                mtxtdesde.Visible = false;
+                mtxtdesde.Text = "";
+                lbldesde.Visible = false;
+                mtxthasta.Visible = false;
+                mtxthasta.Text = "";
+                lblhasta.Visible = false;
+            }
+        }
+    }
+}
---
+++ Funciones.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Data.SqlClient;
+using System.Configuration;
+using static Conexion.Conexionbd;
+using System.Data;
+
+namespace Conexion
+{
+    class Funciones
+    {
+        public void Listadostockprecios(DataGridView dataGrid)
+        {
+            SqlConnection cnn = DbConnection.getDBConnection();
+            DataSet ds = new DataSet();
+            string consulta = "sp_Listado_stock_precios";
+            SqlDataAdapter da = new SqlDataAdapter(consulta, cnn);
+
+            da.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            da.Fill(ds, "consulta");
+            cnn.Close();
+
+            dataGrid.DataSource = ds;
+            dataGrid.DataMember = "consulta";
+
+        }
+
+        public void Pedidosdeclientes(MaskedTextBox desde, MaskedTextBox Hasta, DataGridView datagrid)
+        {
+            //llamo al store
+            string store;
+            store = "sp_pedidos_de_clientes";
+            SqlConnection cnn = DbConnection.getDBConnection();
+            SqlDataAdapter da = new SqlDataAdapter(store, cnn);
+            DataSet ds = new DataSet();
+
+            //parametros
+            da.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            da.SelectCommand.Parameters.AddWithValue("@Desde", desde.Text.ToString());
+            da.SelectCommand.Parameters.AddWithValue("@Hasta", Hasta.Text.ToString());
+            da.Fill(ds, "store");
+            cnn.Close();
+
+            //mostrar en tabla
+            datagrid.DataSource = ds;
+            datagrid.DataMember = "store";
+
+        }
+    }
+}
